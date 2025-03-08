6ed8dc46c1f33ea8e4941e8402b13c46e230b167
msalarkhan10 <msalarkhan10@gmail.com>
2025-03-08 13:05:46 +0500

    complete pythonl2

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import random
 human_choice = input("Choose rock, paper or scissors: " )
 print(human_choice)
+human_score = 0
 choices = ["rock", "paper", "scissors"]
+computer_choice = random.choice(choices)
+print(computer_choice)
+computer_score = 0
+if human_choice == "rock":
+    print("You chose rock")
+    if computer_choice == "paper":
+        print("Computer chose paper")
+        print("You lost to a computer imagine")
+        computer_score += 1
+    elif computer_choice == "scissor":
+        print("Computer chose scissors")
+        print("Player won")
+        human_score += 1
+    else:
+        print("Computer chose rock") 
+        print("Its a tie")         
